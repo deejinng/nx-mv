d036948caae3debd80da5335145be4b0cea6f9c1
Replit user <>
2022-12-31 19:44:55 +0000

    Fixed Taailwiindcss Shit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,11 +7,13 @@ export default function Home() {
         <title>Next.js Movie App</title>
         <meta name="description" content="Movie App with Next.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />      
+            
       </Head>
       <main className="relative">
-        <h1 className="text-3xl font-700 underline">Hello world!</h1>
+        <h1 className="text-red-600 underline text-5xl">Hello world!</h1>
       </main>
     </>
+        
   );
 }
