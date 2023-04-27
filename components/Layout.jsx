919dd806ffa5bf29126a2af91530b67fa82f25e0
deejinng <107402108+deejinng@users.noreply.github.com>
2023-04-27 13:28:18 +0000

    Fixed the header warning

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,15 +4,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Next MV App...</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+        <title>Next App</title>  
       </Head>
-      <main className="font-[600px] bg-red-600">{children}</main>
+      <main className="container">{children}</main>
     </>
   );
 }
